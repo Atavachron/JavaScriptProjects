6557aea14303929a01048d89563d0f0bea1ecb0c
Atavachron <vladimir.kalichuk@gmail.com>
2020-01-30 01:38:51 +0400

    Added functionality to add items

--- ShoppingForm/script.js
+++ ShoppingForm/script.js
@@ -1,54 +1,37 @@
-body {
-  min-height: 100vh;
-  display: grid;
-  align-items: start;
-  justify-items: center;
+const shoppingForm = document.querySelector(".shopping");
+const list = document.querySelector(".list");
+
+const items = [];
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const name = e.currentTarget.item.value;
+  if (!name) return;
+  const item = {
+    name,
+    id: Date.now(),
+    complete: false
+  };
+
+  items.push(item);
+  console.log(`There are now ${items.length} items in your list`);
+
+  e.target.reset();
+  displayItems();
 }
 
-/* Shopping Form */
-.shopping {
-  display: grid;
-  grid-template-columns: 1fr auto;
+function displayItems() {
+  const html = items
+    .map(
+      item => `<li class="shopping-item">
+      <input type="checkbox">
+      <span class="itemName">${item.name}</span>
+      <button aria-label="Remove ${item.name}">&times;</button>
+  </li>`
+    )
+    .join("");
+  console.log(html);
+  list.innerHTML = html;
 }
 
-.shopping-list {
-  background: white;
-  padding: 3rem;
-  border-radius: 1rem;
-  width: 500px;
-  margin: 4rem 0;
-}
-
-.shopping-list ul {
-  list-style: none;
-  margin: 0;
-  padding: 0;
-}
-
-.shopping-item {
-  padding: 1rem 0;
-  display: grid;
-  grid-template-columns: auto 1fr auto;
-  grid-gap: 1rem;
-  align-items: center;
-  border-bottom: 1px solid var(--gray);
-}
-
-.shopping-item input[type="checkbox"] {
-  margin-right: 1rem;
-}
-
-.shopping-item input[type="checkbox"]:checked + .itemName {
-  opacity: 0.5;
-}
-
-.shopping-item button {
-  padding: 0;
-  font-size: 1rem;
-  cursor: pointer;
-  transition: transform 0.2s;
-}
-
-.shopping-item button:hover {
-  transform: scale(1.4);
-}
+shoppingForm.addEventListener("submit", handleSubmit);
