2ac48f02cc9034dbeabee425820e320df7e3fe24
Atavachron <vladimir.kalichuk@gmail.com>
2020-01-30 00:57:17 +0400

    Added JS code

---
+++ FaceDetectionCensoring/script.js
@@ -0,0 +1,88 @@
+const video = document.querySelector(".webcam");
+const canvas = document.querySelector(".video");
+const ctx = canvas.getContext("2d");
+
+const faceCanvas = document.querySelector(".face");
+const faceCtx = faceCanvas.getContext("2d");
+const faceDetector = new window.FaceDetector();
+const optionsInputs = document.querySelectorAll(
+  '.controls input[type="range"]'
+);
+
+console.log(optionsInputs);
+
+const options = {
+  SIZE: 10,
+  SCALE: 1
+};
+
+optionsInputs.forEach(input => input.addEventListener("input", handleOption));
+
+function handleOption(event) {
+  const { value, name } = event.currentTarget;
+  options[name] = parseFloat(value);
+}
+
+async function populateVideo() {
+  const stream = await navigator.mediaDevices.getUserMedia({
+    video: { width: 1000, height: 600 }
+  });
+  video.srcObject = stream;
+  await video.play();
+
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  faceCanvas.width = video.videoWidth;
+  faceCanvas.height = video.videoHeight;
+}
+
+async function detect() {
+  const faces = await faceDetector.detect(video);
+  faces.forEach(drawFace);
+  faces.forEach(censor);
+  requestAnimationFrame(detect);
+}
+
+function drawFace(face) {
+  const { width, height, top, left } = face.boundingBox;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.strokeStyle = "#ffc600";
+  ctx.lineWidth = 2;
+  ctx.strokeRect(left, top, width, height);
+}
+
+function censor({ boundingBox: face }) {
+  faceCtx.imageSmoothingEnabled = false;
+  faceCtx.clearRect(0, 0, faceCanvas.width, faceCanvas.height);
+
+  //draw a small face
+  faceCtx.drawImage(
+    video,
+    face.x,
+    face.y,
+    face.width,
+    face.height,
+    face.x,
+    face.y,
+    options.SIZE,
+    options.SIZE
+  );
+
+  //stretch the small face
+  const width = face.width * options.SCALE;
+  const height = face.height * options.SCALE;
+
+  faceCtx.drawImage(
+    faceCanvas,
+    face.x,
+    face.y,
+    options.SIZE,
+    options.SIZE,
+    face.x - (width - face.width) / 2,
+    face.y - (height - face.height) / 2,
+    width,
+    height
+  );
+}
+
+populateVideo().then(detect);
