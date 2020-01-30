cc9e20e51c70dcf9d2a5eca581f1615a363f561e
Atavachron <vladimir.kalichuk@gmail.com>
2020-01-30 13:44:51 +0400

    Completed JS code and added CSS

--- ShoppingForm/script.js
+++ ShoppingForm/script.js
@@ -1,7 +1,7 @@
 const shoppingForm = document.querySelector(".shopping");
 const list = document.querySelector(".list");
 
-const items = [];
+let items = [];
 
 function handleSubmit(e) {
   e.preventDefault();
@@ -17,21 +17,61 @@ function handleSubmit(e) {
   console.log(`There are now ${items.length} items in your list`);
 
   e.target.reset();
-  displayItems();
+
+  list.dispatchEvent(new CustomEvent("itemsUpdated"));
 }
 
 function displayItems() {
   const html = items
     .map(
       item => `<li class="shopping-item">
-      <input type="checkbox">
+      <input value="${item.id}" type="checkbox" ${item.complete && "checked"}>
       <span class="itemName">${item.name}</span>
-      <button aria-label="Remove ${item.name}">&times;</button>
+      <button aria-label="Remove ${item.name}" value="${
+        item.id
+      }">&times;</button>
   </li>`
     )
     .join("");
-  console.log(html);
   list.innerHTML = html;
 }
 
+function mirrorToLocalStorage() {
+  localStorage.setItem("items", JSON.stringify(items));
+}
+
+function restoreFromLocalStorage() {
+  const lsItems = JSON.parse(localStorage.getItem("items"));
+  if (lsItems.length) {
+    items.push(...lsItems);
+    list.dispatchEvent(new CustomEvent("itemsUpdated"));
+  }
+}
+
+function deleteItem(id) {
+  console.log("deleted", id);
+  items = items.filter(item => item.id !== id);
+  list.dispatchEvent(new CustomEvent("itemsUpdated"));
+}
+
+function markAsComplete(id) {
+  const itemRef = items.find(item => item.id === id);
+  itemRef.complete = !itemRef.complete;
+  list.dispatchEvent(new CustomEvent("itemsUpdated"));
+}
+
 shoppingForm.addEventListener("submit", handleSubmit);
+list.addEventListener("itemsUpdated", displayItems);
+list.addEventListener("itemsUpdated", mirrorToLocalStorage);
+
+list.addEventListener("click", event => {
+  const id = parseInt(event.target.value);
+  if (event.target.matches("button")) {
+    deleteItem(id);
+  }
+  if (event.target.matches('input[type="checkbox"]')) {
+    markAsComplete(id);
+  }
+});
+
+restoreFromLocalStorage();
