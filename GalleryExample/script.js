685c4dca538427c35f8b7aadb884bdef139dee07
Atavachron <vladimir.kalichuk@gmail.com>
2020-02-07 11:08:16 +0400

    Amended html and completed JS

--- GalleryExample/script.js
+++ GalleryExample/script.js
@@ -9,6 +9,45 @@ function Gallery(gallery) {
   const nextButton = modal.querySelector(".next");
   let currentImage; //To track the currently opened image
 
+  function openModal() {
+    if (modal.matches(".open")) {
+      console.info("Modal is already open");
+      return;
+    }
+    modal.classList.add("open");
+
+    //Event Listeners that will be bound when the model is opened
+    window.addEventListener("keyup", handleKeyUp);
+    nextButton.addEventListener("click", showNextImage);
+    prevButton.addEventListener("click", showPreviousImage);
+  }
+
+  function closeModal() {
+    modal.classList.remove("open");
+
+    window.removeEventListener("keyup", handleKeyUp);
+    nextButton.removeEventListener("click", showNextImage);
+    prevButton.removeEventListener("click", showPreviousImage);
+  }
+
+  function handleClickOutside(e) {
+    if (e.target === e.currentTarget) {
+      closeModal();
+    }
+  }
+
+  function handleKeyUp(event) {
+    if (event.key === "Escape") {
+      return closeModal();
+    }
+    if (event.key === "ArrowRight") {
+      return showNextImage();
+    }
+    if (event.key === "ArrowLeft") {
+      return showPreviousImage();
+    }
+  }
+
   function showImage(el) {
     if (!el) {
       console.info("There is no image to display");
@@ -18,6 +57,16 @@ function Gallery(gallery) {
     modal.querySelector("h2").textContent = el.title;
     modal.querySelector("figure p").textContent = el.dataset.description;
     currentImage = el;
+
+    openModal();
+  }
+
+  function showNextImage() {
+    showImage(currentImage.nextElementSibling || gallery.firstElementChild);
+  }
+
+  function showPreviousImage() {
+    showImage(currentImage.previousElementSibling || gallery.lastElementChild);
   }
 
   images.forEach(image =>
@@ -25,6 +74,16 @@ function Gallery(gallery) {
       showImage(event.currentTarget);
     })
   );
+
+  images.forEach(image => {
+    image.addEventListener("keyup", e => {
+      if (e.key === "Enter") {
+        showImage(e.currentTarget);
+      }
+    });
+  });
+
+  modal.addEventListener("click", handleClickOutside);
 }
 
 const gallery1 = Gallery(document.querySelector(".gallery1"));
