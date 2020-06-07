3c49a8ff23791ff2bc10a55cb4dbb12558138ced
Atavachron <37329317+Atavachron@users.noreply.github.com>
2020-06-07 13:40:36 +0400

    Add input validation and alert message

--- BookListApp/script.js
+++ BookListApp/script.js
@@ -12,7 +12,8 @@ function UI() {}
 //Instantiate a UI object
 const ui = new UI();
 
-//Add UI methods to the UI prototype
+//UI prototype methods
+
 UI.prototype.addBookToList = function (book) {
   const list = document.querySelector('#book-list');
 
@@ -34,6 +35,23 @@ UI.prototype.clearInputFields = function () {
   document.querySelector('#isbn').value = '';
 };
 
+UI.prototype.showAlert = function (msg, className) {
+  //Create a new element where the alert will be displayed
+  const div = document.createElement('p');
+  //Give a class name to the div
+  div.className = `alert ${className}`;
+  //Create a text node and append to the div
+  div.appendChild(document.createTextNode(msg));
+  //Insert the div into the DOM
+  const form = document.querySelector('#book-form');
+  form.insertAdjacentElement('beforebegin', div);
+
+  //Remove the alert after a time out
+  setTimeout(() => {
+    document.querySelector('.alert').remove();
+  }, 3000);
+};
+
 document.querySelector('#book-form').addEventListener('submit', (e) => {
   //Get the input values
   const title = document.querySelector('#title').value;
@@ -44,8 +62,13 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
   const book = new Book(title, author, isbn);
 
   //Call the method from the UI prototype
-  ui.addBookToList(book);
-  ui.clearInputFields();
+  if (title === '' || author === '' || isbn === '') {
+    ui.showAlert('Please make sure all fields are filled', 'error');
+  } else {
+    ui.addBookToList(book);
+    ui.clearInputFields();
+    ui.showAlert('Book added', 'success');
+  }
 
   e.preventDefault();
 });
