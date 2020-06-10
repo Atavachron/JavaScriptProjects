55b0bd01474943525dbf53eaa2571a8fc2da72a9
Atavachron <37329317+Atavachron@users.noreply.github.com>
2020-06-10 21:52:18 +0400

    Refactor as ES6

--- FetchSandbox/script.js
+++ FetchSandbox/script.js
@@ -1,54 +1,36 @@
 //Fetching data from a local text file
-document.getElementById('fetchText').addEventListener('click', function () {
+document.getElementById('fetchText').addEventListener('click', () => {
   fetch('text.txt')
     .then(handleErrors)
-    .then(function (res) {
-      return res.text();
-    })
-    .then(function (text) {
-      document.querySelector('.output').innerHTML = text;
-    })
-    .catch(function (err) {
-      console.log(err);
-    });
+    .then((res) => res.text())
+    .then((text) => (document.querySelector('.output').innerHTML = text))
+    .catch((err) => console.log(err));
 });
 
 //Fetching data from local JSON file
-document.getElementById('fetchJSON').addEventListener('click', function () {
+document.getElementById('fetchJSON').addEventListener('click', () => {
   fetch('json.JSON')
     .then(handleErrors)
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (res) {
+    .then((res) => res.json())
+    .then((res) => {
       let output = '';
-      res.forEach(function (person) {
-        output += `<li>${person.name}</li>`;
-      });
+      res.forEach((person) => (output += `<li>${person.name}</li>`));
       document.querySelector('.output').innerHTML = output;
     })
-    .catch(function (err) {
-      console.log(err);
-    });
+    .catch((err) => console.log(err));
 });
 
 //Fetching the emails of all users from https://reqres.in/api/users
-document.getElementById('fetchRemote').addEventListener('click', function () {
+document.getElementById('fetchRemote').addEventListener('click', () => {
   fetch('https://reqres.in/api/users')
     .then(handleErrors)
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (res) {
+    .then((res) => res.json())
+    .then((res) => {
       let output = '';
-      res.data.forEach(function (entry) {
-        output += `<li>${entry.email}</li>`;
-      });
+      res.data.forEach((entry) => (output += `<li>${entry.email}</li>`));
       document.querySelector('.output').innerHTML = output;
     })
-    .catch(function (err) {
-      console.log(err);
-    });
+    .catch((err) => console.log(err));
 });
 
 function handleErrors(res) {
