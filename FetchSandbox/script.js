f7ddc3f30b4523b3361f32fe7d4831b20a62ca32
Atavachron <37329317+Atavachron@users.noreply.github.com>
2020-06-10 22:04:30 +0400

    Add formatting changes

--- FetchSandbox/script.js
+++ FetchSandbox/script.js
@@ -2,35 +2,35 @@
 document.getElementById('fetchText').addEventListener('click', () => {
   fetch('text.txt')
     .then(handleErrors)
-    .then((res) => res.text())
-    .then((text) => (document.querySelector('.output').innerHTML = text))
-    .catch((err) => console.log(err));
+    .then(res => res.text())
+    .then(text => (document.querySelector('.output').innerHTML = text))
+    .catch(err => console.log(err));
 });
 
 //Fetching data from local JSON file
 document.getElementById('fetchJSON').addEventListener('click', () => {
   fetch('json.JSON')
     .then(handleErrors)
-    .then((res) => res.json())
-    .then((res) => {
+    .then(res => res.json())
+    .then(res => {
       let output = '';
-      res.forEach((person) => (output += `<li>${person.name}</li>`));
+      res.forEach(person => (output += `<li>${person.name}</li>`));
       document.querySelector('.output').innerHTML = output;
     })
-    .catch((err) => console.log(err));
+    .catch(err => console.log(err));
 });
 
 //Fetching the emails of all users from https://reqres.in/api/users
 document.getElementById('fetchRemote').addEventListener('click', () => {
   fetch('https://reqres.in/api/users')
     .then(handleErrors)
-    .then((res) => res.json())
-    .then((res) => {
+    .then(res => res.json())
+    .then(res => {
       let output = '';
-      res.data.forEach((entry) => (output += `<li>${entry.email}</li>`));
+      res.data.forEach(entry => (output += `<li>${entry.email}</li>`));
       document.querySelector('.output').innerHTML = output;
     })
-    .catch((err) => console.log(err));
+    .catch(err => console.log(err));
 });
 
 function handleErrors(res) {
