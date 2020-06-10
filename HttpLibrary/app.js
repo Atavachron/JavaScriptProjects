a6fa5dfe99d326973431fb01c644f3d421bd0968
Atavachron <37329317+Atavachron@users.noreply.github.com>
2020-06-10 13:38:17 +0400

    Add post put delete methods to library

--- HttpLibrary/app.js
+++ HttpLibrary/app.js
@@ -3,10 +3,39 @@
 
 const http = new EasyHTTP();
 
-http.get('https://jsonplaceholder.typicode.com/users', function (err, users) {
+// http.get('https://reqres.in/api/users', function (err, users) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(users);
+//   }
+// });
+
+const data = {
+  name: 'Tom',
+  job: 'astronaut',
+};
+
+// http.post('https://reqres.in/api/users', data, function (err, response) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(response);
+//   }
+// });
+
+// http.put('https://reqres.in/api/users/1', data, function (err, response) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(response);
+//   }
+// });
+
+http.delete('https://reqres.in/api/users/1', function (err, response) {
   if (err) {
     console.log(err);
   } else {
-    console.log(users);
+    console.log(response);
   }
 });
--- HttpLibrary/httplib.js
+++ HttpLibrary/httplib.js
@@ -22,3 +22,37 @@ EasyHTTP.prototype.get = function (url, callback) {
 
   this.xhr.send();
 };
+
+EasyHTTP.prototype.post = function (url, data, callback) {
+  this.xhr.open('POST', url, true);
+  this.xhr.setRequestHeader('Content-Type', 'application/json');
+  let self = this;
+  this.xhr.onload = function () {
+    callback(null, self.xhr.response);
+  };
+
+  this.xhr.send(JSON.stringify(data));
+};
+
+EasyHTTP.prototype.put = function (url, data, callback) {
+  this.xhr.open('PUT', url, true);
+  this.xhr.setRequestHeader('Content-Type', 'application/json');
+  let self = this;
+  this.xhr.onload = function () {
+    callback(null, self.xhr.response);
+  };
+  this.xhr.send(JSON.stringify(data));
+};
+
+EasyHTTP.prototype.delete = function (url, callback) {
+  this.xhr.open('DELETE', url, true);
+  let self = this;
+  this.xhr.onload = function () {
+    if (self.xhr.status === 200 || self.xhr.status === 204) {
+      callback(null, 'Entry Deleted');
+    } else {
+      callback('Error:' + self.xhr.status);
+    }
+  };
+  this.xhr.send();
+};
