27f9dbd7b49282d1bb7f9ab92812eb874fcc497f
Atavachron <37329317+Atavachron@users.noreply.github.com>
2020-06-14 20:23:01 +0400

    Use modal to change location

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -1,7 +1,20 @@
 const weather = new Weather('Dubai', 'AE');
 const ui = new UI();
 
+//Load the weather automatically when the page is loaded
 document.addEventListener('DOMContentLoaded', getWeather);
+
+//Get the values from the modal
+document.getElementById('change-btn').addEventListener('click', e => {
+  const city = document.getElementById('city').value;
+  const country = document.getElementById('country').value;
+
+  weather.changeLocation(city, country);
+  getWeather();
+
+  //Use JQuery to hide modal
+  $('#locModal').modal('hide');
+});
 
 function getWeather() {
   weather
--- WeatherApp/ui.js
+++ WeatherApp/ui.js
@@ -3,7 +3,6 @@ class UI {
     this.location = document.getElementById('location');
     this.description = document.getElementById('description');
     this.temperature = document.getElementById('temperature');
-    this.details = document.getElementById('details');
     this.icon = document.getElementById('icon');
     this.humidity = document.getElementById('humidity');
     this.feelsLike = document.getElementById('feels-like');
